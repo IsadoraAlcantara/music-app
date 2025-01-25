e49f3c70e5304e679a2b6f5afe337f985b69eca0
IsadoraAlcantara <Isadoraalca405@gmail.com>
2025-01-25 14:50:23 -0300

    CHORE: finalização da side bar sem rotas

---
+++ src/components/icons/index.js
@@ -0,0 +1,9 @@
+import HomeOutline from 'vue-material-design-icons/HomeOutline.vue'
+import HeartOutline from 'vue-material-design-icons/HeartOutline.vue'
+import PlaylistPlus from 'vue-material-design-icons/PlaylistPlus.vue'
+
+export {
+    HomeOutline,
+    HeartOutline,
+    PlaylistPlus,
+}
--- src/components/template/large/LargeAside.vue
+++ src/components/template/large/LargeAside.vue
@@ -1,6 +1,5 @@
 <script setup>
-import HomeOutline from 'vue-material-design-icons/HomeOutline.vue'
-import HeartOutline from 'vue-material-design-icons/HeartOutline.vue'
+import { HomeOutline, HeartOutline, PlaylistPlus } from '../../icons'
 </script>
 <template>
   <main>
@@ -10,8 +9,50 @@ import HeartOutline from 'vue-material-design-icons/HeartOutline.vue'
         alt=""
       />
     </div>
-    <HomeOutline />
-    <HeartOutline />
+    <div class="navigation">
+      <div class="line">
+        <HomeOutline />
+        <p>Início</p>
+      </div>
+      <div class="line">
+        <HeartOutline />
+        <p>Gêneros</p>
+      </div>
+    </div>
+    <div class="navigation">
+      <h2>Biblioteca</h2>
+      <div class="line">
+        <p>Descobrir</p>
+      </div>
+      <div class="line">
+        <p>Playlists</p>
+      </div>
+      <div class="line">
+        <p>Faixas</p>
+      </div>
+      <div class="line">
+        <p>Artistas</p>
+      </div>
+      <div class="line">
+        <p>Álbuns</p>
+      </div>
+      <div class="line">
+        <p>Importar músicas</p>
+      </div>
+    </div>
+    <div class="navigation">
+      <h2>Playlists</h2>
+      <div class="line">
+        <PlaylistPlus />
+        <p>Criar playlist</p>
+      </div>
+      <div class="line">
+        <p>Phonk</p>
+      </div>
+      <div class="line">
+        <p>Triste</p>
+      </div>
+    </div>
   </main>
 </template>
 <style scoped>
@@ -29,5 +70,27 @@ main {
 
 .logo img {
   width: 150px;
+}
+
+.navigation {
+  margin: 10px 0;
+}
+
+.line {
+  color: var(--grey-200);
+  font-weight: 500;
+  display: flex;
+  gap: 15px;
+  margin: 10px 0;
+}
+
+.line:hover {
+  color: var(--green);
+}
+
+h2 {
+  color: var(--white);
+  font-size: 16px;
+  font-weight: 500;
 }
 </style>
