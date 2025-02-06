69c5998ed0706881140b8654a5a3a7305978f7f1
IsadoraAlcantara <Isadoraalca405@gmail.com>
2025-02-06 14:36:14 -0300

    FEAT: criação da listagem de gêneros na view de gêneros

--- src/components/Gender/MainGender.vue
+++ src/components/Gender/MainGender.vue
@@ -1,16 +1,72 @@
 <script setup>
+import { genders } from './index'
 </script>
 <template>
-    <main>
-        a
-    </main>
+  <div class="container">
+    <h1>Gêneros</h1>
+    <div class="buttons">
+      <button class="activated">Tudo</button><button class="default">Favoritos</button>
+    </div>
+    <div class="cards">
+      <div v-for="gender in genders">
+        {{ gender.name }} {{ gender.url }}
+      </div>
+    </div>
+  </div>
 </template>
 <style scoped>
-main {
-  background-color: #1E1E1E;
+.cards {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 20px;
+
+  & div {
+    width: 14.5vw;
+    height: 8vw;
+    background-color: blue;
+    border-radius: 7px;
+    padding: 15px;
+    font-weight: 900;
+    font-size: 23px;
+  }
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  padding: 20px;
+  background-color: #1e1e1e;
   margin-right: 10px;
   width: 100%;
   height: 750px;
   border-radius: 7px;
+  color: #e6e8ee;
+  overflow-y: scroll;
+  max-width: 63.8vw;
+}
+
+.buttons {
+  display: flex;
+  gap: 7px;
+
+  & button {
+    height: 30px;
+    width: 80px;
+    border-radius: 7px;
+    font-weight: 600;
+  }
+
+  & .activated {
+    background-color: #e6e8ee;
+    color: #0d0d0d;
+    border: none;
+  }
+
+  & .default {
+    border: #505050 solid 1px;
+    background-color: transparent;
+    color: #e6e8ee;
+  }
 }
 </style>
---
+++ src/components/Gender/index.js
@@ -0,0 +1,26 @@
+const genders = [
+  {id: 1, name:'Pop', color:'#6092FF'},
+  {id: 2, name:'Clássica', color:'#C9A8A8'},
+  {id: 3, name:'Jazz', color:'#32BD57'},
+  {id: 4, name:'Rock', color:'#32BD57'},
+  {id: 5, name:'Hip Hop', color:'#32BD57'},
+  {id: 6, name:'Funk', color:'#32BD57'},
+  {id: 7, name:'Country', color:'#32BD57'},
+  {id: 8, name:'MPB', color:'#32BD57'},
+  {id: 9, name:'Indie', color:'#32BD57'},
+  {id: 10, name:'Samba', color:'#32BD57'},
+  {id: 11, name:'Forró', color:'#6092FF'},
+  {id: 12, name:'Axé', color:'#C9A8A8'},
+  {id: 13, name:'Indie Alternativo', color:'#32BD57'},
+  {id: 14, name:'Blues', color:'#32BD57'},
+  {id: 15, name:'Eletrônica', color:'#32BD57'},
+  {id: 16, name:'Gospel', color:'#32BD57'},
+  {id: 17, name:'Sertanejo', color:'#32BD57'},
+  {id: 18, name:'Sertanejo Universitário', color:'#32BD57'},
+  {id: 19, name:'Arrocha', color:'#32BD57'},
+  {id: 20, name:'Sofrência', color:'#32BD57'},
+]
+
+export {
+    genders
+}
